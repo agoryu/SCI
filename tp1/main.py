a520b4ef7882f71b258284f216f18d1853cce7a7
Vanegue Elliot <vanegue@b02p11.fil.univ-lille1.fr>
2015-09-15 17:26:33 +0200

    ajout readme

--- tp1/main.py
+++ tp1/main.py
@@ -1,16 +1,19 @@
 # On importe Tkinter
 from tkinter import *
 
+#taille fenetre
+tailleX = sys.argv[1]
+tailleY = sys.argv[2]
+tailleCase = sys.argv[3]
+
+ralentisseur = sys.argv[4]
+nbBille = sys.argv[5]
+
+torique = sys.argv[6]
+
 # On crée une fenêtre, racine de notre interface
 fenetre = Tk()
 
-# On crée un label (ligne de texte) souhaitant la bienvenue
-# Note : le premier paramètre passé au constructeur de Label est notre
-# interface racine
-champ_label = Label(fenetre, text="Salut les Zér0s !")
-
-# On affiche le label dans la fenêtre
-champ_label.pack()
-
+environnementG=Canvas(fenetre, width=tailleX*tailleCase, height=tailleY*tailleCase, backgroud='white')
 # On démarre la boucle Tkinter qui s'interompt quand on ferme la fenêtre
 fenetre.mainloop()
