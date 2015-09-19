2f9d0cbf23e5649f6b3a7c99cb44f3bda8b86cb3
Gaëtan DEFLANDRE <gaetan.deflandre@gmail.com>
2015-09-19 15:08:19 +0200

    fix nom de variable

--- tp1/SMA.py
+++ tp1/SMA.py
@@ -5,18 +5,18 @@ class SMA:
     def __init__(self, environnement, environnementG):
         self.environnement = environnement
         self.environnementG = environnementG
-        self.agent = []
+        self.agents = []
         self.nbAgent = 0
 
     def run(self, nbTours):
-        for a in self.agent:
+        for a in self.agents:
             a.decide()
             #voir pour executer les changement dans la fenetre
             environnementG.drawEnvG(self)
 
     def addAgent(self, a):
         self.environnement[a.x][a.y] = a
-        self.agent.append(a)
+        self.agents.append(a)
 
     def isFree(self, x, y):
         return not self.environnement[x][y].isAgent()
