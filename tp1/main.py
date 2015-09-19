27ee33f5baec80f2639a00c9cb0c29b0d66e578c
Gaëtan DEFLANDRE <gaetan.deflandre@gmail.com>
2015-09-19 15:48:10 +0200

    fix indent

--- tp1/main.py
+++ tp1/main.py
@@ -18,19 +18,20 @@ for i in range(0, tailleX-1):
     environnement.append([])
     for j in range(0, tailleY-1):
         environnement[i].append([EmptyCell(i,j)])
+        
 fenetre = Tk()
 ############### lancement de la simulation ##################
 environnementG = EnvironmentG(fenetre, tailleX, tailleY, tailleCase)
 sma = SMA(environnement, environnementG)
 
 for i in range(0, nbBille):
-	x = 0
-	y = 0
-	while(not sma.isFree(x,y)):
-		x = random.randint(0, tailleX)
-		y = random.randint(0, tailleY)
-	sma.addAgent(Agent(x, y, 0, 0))
-sma.run(1000)
+    x = 0
+    y = 0
+    while(not sma.isFree(x,y)):
+        x = random.randint(0, tailleX)
+        y = random.randint(0, tailleY)
+        sma.addAgent(Agent(x, y, 0, 0))
+        sma.run(1000)
 
 # On démarre la boucle Tkinter qui s'interompt quand on ferme la fenêtre
 fenetre.mainloop()
