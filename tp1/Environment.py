a4833b803b2ece3fd9447c605973c7bb9d2025b5
Gaëtan DEFLANDRE <gaetan.deflandre@gmail.com>
2015-09-19 15:38:36 +0200

    classe Environment completé

--- tp1/Environment.py
+++ tp1/Environment.py
@@ -1,3 +1,7 @@
+from Cell import Cell
+from EmptyCell import EmptyCell
+from Agent import Agent
+
 class Environment:
 
     def __init__(self, lengthX, lengthY):
@@ -30,3 +34,45 @@ class Environment:
         self.lengthY = lengthY
         self.grid = []
         
+        for x in xrange(lengthX):
+            self.grid.append([])
+            for y in xrange(lengthY):
+                self.grid[x].append(EmptyCell(x, y))
+
+
+    def getCell(self, x, y):
+        """
+        @param x: position en x
+        @param y: position en y
+        @return: la case en x et y de l'environnement
+        """
+        return self.grid[x][y]
+
+
+    def setEmptyCell(self, x, y):
+        """
+        Fixe une nouvelle case vide en x et y.
+        @param x: position en x
+        @param y: position en y
+        """
+        self.grid[x][y] = EmptyCell(x,y)
+
+
+    def setAgent(self, x, y, a):
+        """
+        Met l'agent a dans la case en x et y.
+        @param x: position en x
+        @param y: position en y
+        @param a: l'agent à mettre dans l'environnement.
+        """
+        if (not a.isAgent()):
+            raise ValueError("a n'est pas agente")
+        else:
+            self.grid[x][y] = a
+
+
+    def getGrid(self):
+        """
+        @return: la grille de l'environnement
+        """
+        return self.grid
