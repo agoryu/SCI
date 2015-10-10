240b93710ef8503198d4349131563acf21a032ee
elliot <elliot@localhost.localdomain>
2015-10-10 11:52:42 +0200

    dijsktra pour 1 hunted ok + debut decide de hunter

--- tp1/Hunter.py
+++ tp1/Hunter.py
@@ -6,5 +6,29 @@ class Hunter(Agent):
         Agent.__init__(self, x, y, 0, 0)
         self.color = 'green'
 
-    def decide(self, sma):
+    def decide(saelf, sma):
+        env = sma.getEnv()
+        dijsktra = sma.dijsktra
+        tab = []
+
+        if(sma.isFree(self.x+1, self.y)):
+            tab.append(dijsktra[self.x+1][self.y])
+        if(sma.isFree(self.x-1, self.y)):
+            tab.append(dijsktra[self.x-1][self.y])
+        if(sma.isFree(self.x, self.y+1)):
+            tab.append(dijsktra[self.x][self.y+1])
+        if(sma.isFree(self.x, self.y-1)):
+            tab.append(dijsktra[self.x][self.y-1])
+
+        mini = min(tab)
+
+        for i in tab:
+            if(i == min):
+                
         return
+
+    def isHunter(self):
+        return True
+
+    def isHunted(self):
+        return False
