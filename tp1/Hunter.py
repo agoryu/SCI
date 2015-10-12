abed889d5833edb6ffce1728ee74a78b8e4fff29
Vanegue Elliot <vanegue@b02p11.fil.univ-lille1.fr>
2015-10-12 14:30:20 +0200

    dijsktra pour tous les hunters

--- tp1/Hunter.py
+++ tp1/Hunter.py
@@ -20,25 +20,27 @@ class Hunter(Agent):
         x = self.x
         y = self.y
 
-        if(sma.isFree(self.x+1, self.y)):
-            mini = dijsktra[self.x+1][self.y]
-            x = self.x+1
-            y = self.y
-        if(sma.isFree(self.x-1, self.y)):
-            if(dijsktra[self.x-1][self.y] < mini):
-                mini = dijsktra[self.x-1][self.y]
-                x = self.x-1
-                y = self.y
-        if(sma.isFree(self.x, self.y+1)):
-            if(dijsktra[self.x][self.y+1] < mini):
-                mini = dijsktra[self.x][self.y+1]
-                x = self.x
-                y = self.y+1
-        if(sma.isFree(self.x, self.y-1)):
-            if(dijsktra[self.x][self.y-1] < mini):
-                mini = dijsktra[self.x][self.y-1]
-                x = self.x
-                y = self.y-1
+        for d in dijsktra:
+            if(sma.isFree(self.x+1, self.y)):
+                if(d[self.x+1][self.y] < mini):
+                    mini = d[self.x+1][self.y]
+                    x = self.x+1
+                    y = self.y
+            if(sma.isFree(self.x-1, self.y)):
+                if(d[self.x-1][self.y] < mini):
+                    mini = d[self.x-1][self.y]
+                    x = self.x-1
+                    y = self.y
+            if(sma.isFree(self.x, self.y+1)):
+                if(d[self.x][self.y+1] < mini):
+                    mini = d[self.x][self.y+1]
+                    x = self.x
+                    y = self.y+1
+            if(sma.isFree(self.x, self.y-1)):
+                if(d[self.x][self.y-1] < mini):
+                    mini = d[self.x][self.y-1]
+                    x = self.x
+                    y = self.y-1
 
         env.setAgent(x, y, self)
         env.setEmptyCell(self.x, self.y)
